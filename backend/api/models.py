7e2bf84cccd855100572883b4705dddf95d39940
Evgeniy Khlystov <hlystovea@gmail.com>
2021-07-06 23:31:02 +0700

    Ingredient model have been created

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Ингредиенты',
+        max_length=200,
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единицы измерения',
+        max_length=200,
+    )
+
+    class Meta:
+        app_label = 'api'
+        ordering = ('name',)
+        verbose_name = _('Ингредиент')
+        verbose_name_plural = _('Ингредиенты')
+
+    def __str__(self):
+        return self.name
