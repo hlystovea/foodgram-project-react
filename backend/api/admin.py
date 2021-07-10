b5beb58f3868fd5d49c84a510ce4212bcae7148c
Evgeniy Khlystov <hlystovea@gmail.com>
2021-07-10 11:21:23 +0700

    added Ingredient, Quantity, Tag and Recipe models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,97 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from . import models
+
+User = get_user_model()
+
+
+class MixinAdmin(admin.ModelAdmin):
+    empty_value_display = _('-пусто-')
+
+
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'email',
+                    'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+    readonly_fields = ('date_joined', 'last_login')
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name', 'email')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups'),
+        }),
+        (_('Important dates'), {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'user_permissions',
+            }
+        if (
+            not is_superuser
+            and obj is not None
+            and (obj.is_superuser or obj == request.user)
+        ):
+            disabled_fields |= {
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+        return form
+
+
+@admin.register(models.Ingredient)
+class IngredientAdmin(MixinAdmin):
+    list_display = ('id', 'name', 'measurement_unit')
+    search_fields = ('name', )
+    list_filter = ('measurement_unit', )
+
+
+@admin.register(models.Quantity)
+class QuantityAdmin(MixinAdmin):
+    list_display = ('id', 'ingredient', 'quantity')
+    search_fields = ('ingredient', )
+    autocomplete_fields = ('ingredient', )
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(MixinAdmin):
+    list_display = ('id', 'name', 'author', 'cooking_time')
+    search_fields = ('name', 'author', 'tags')
+    list_filter = ('tags', )
+    autocomplete_fields = ('tags', )
+    filter_horizontal = ('ingredients', )
+
+
+@admin.register(models.Tag)
+class TagAdmin(MixinAdmin):
+    list_display = ('id', 'name', 'slug', 'color')
+    search_fields = ('name', 'slug')
+    list_filter = ('color', )
+    prepopulated_fields = {'slug': ('name',)}
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,134 @@
+from django.contrib.auth import get_user_model
+from django.core import validators
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from . import fields
+
+User = get_user_model()
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name=_('Наименование'),
+        max_length=200,
+    )
+    measurement_unit = models.CharField(
+        verbose_name=_('Единица измерения'),
+        max_length=200,
+    )
+
+    class Meta:
+        app_label = 'api'
+        ordering = ('name', )
+        verbose_name = _('Ингредиент')
+        verbose_name_plural = _('Ингредиенты')
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
+
+
+class Quantity(models.Model):
+    ingredient = models.ForeignKey(
+        to=Ingredient,
+        verbose_name=_('Ингредиент'),
+        on_delete=models.CASCADE,
+        related_name='quantities'
+    )
+    quantity = models.PositiveSmallIntegerField(
+        verbose_name=_('Количество'),
+        validators=[
+            validators.MaxValueValidator(
+                10000,
+                message='Слишком много, проверьте единицы измерения',
+            ),
+        ]
+    )
+
+    class Meta:
+        app_label = 'api'
+        ordering = ('ingredient', )
+        verbose_name = _('Кол-во ингредиента')
+        verbose_name_plural = _('Кол-ва ингредиентов')
+
+    def __str__(self):
+        return f'{self.ingredient.name}: {self.quantity} {self.ingredient.measurement_unit}'
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name=_('Наименование'),
+        max_length=200,
+        unique=True,
+    )
+    color = fields.ColorField(
+        verbose_name=_('Цвет'),
+        default='#FF0000'
+    )
+    slug = models.SlugField(
+        verbose_name=_('Слаг'),
+        max_length=200,
+        unique=True,
+    )
+
+    class Meta:
+        app_label = 'api'
+        ordering = ('name', )
+        verbose_name = _('Тег')
+        verbose_name_plural = _('Теги')
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        to=User,
+        verbose_name=_('Автор'),
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    image = models.ImageField(
+        verbose_name=_('Изображение'),
+        upload_to='recipes/',
+    )
+    name = models.CharField(
+        verbose_name=_('Название'),
+        max_length=200,
+        unique=True,
+    )
+    ingredients = models.ManyToManyField(
+        to=Quantity,
+        verbose_name=_('Ингредиенты'),
+        related_name='recipes',
+    )
+    text = models.TextField(
+        verbose_name=_('Рецепт'),
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name=_('Время приготовления, мин'),
+        validators=[
+            validators.MinValueValidator(
+                1,
+                message='Время не может быть меньше 1 минуты',
+            ),
+            validators.MaxValueValidator(
+                1000,
+                message='Слишком долго, укажите время в минутах',
+            ),
+        ]
+    )
+    tags = models.ManyToManyField(
+        to=Tag,
+        verbose_name=_('Теги'),
+        related_name='recipes',
+    )
+
+    class Meta:
+        app_label = 'api'
+        ordering = ('-id', )
+        verbose_name = _('Рецепт')
+        verbose_name_plural = _('Рецепты')
+
+    def __str__(self):
+        return self.name
