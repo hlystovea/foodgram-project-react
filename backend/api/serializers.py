6436a8b577f4b21a7953026a49ae3451d99bb3eb
Evgeniy Khlystov <hlystovea@gmail.com>
2021-07-21 19:24:51 +0700

    added drf_extra_fields

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,4 +1,4 @@
-from drf_base64.fields import Base64ImageField
+from drf_extra_fields.fields import Base64ImageField
 from django.contrib.auth import get_user_model
 from drf_base64.serializers import ModelSerializer
 from rest_framework import serializers, validators
