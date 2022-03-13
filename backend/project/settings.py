23d2df94c7db58a8eb7a90722c6925101a4f040f
Evgenii Khlystov <hlystovea@gmail.com>
2022-03-13 22:06:22 +0700

    Updated ALLOWED_HOSTS

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -8,7 +8,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = int(os.environ.get('DEBUG', False))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*', 'web:8000']
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['https://foodgram.hlystovea.ru']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
